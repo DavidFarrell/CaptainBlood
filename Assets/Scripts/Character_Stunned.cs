b79e8723696ff2d56f0fdbd5e7ff261ed0684ac6
sharpoid <sharpoid@hotmail.co.uk>
2014-01-25 09:42:02 +0000

    Basic stun

--- Assets/Scripts/Character_Stunned.cs
+++ Assets/Scripts/Character_Stunned.cs
@@ -7,9 +7,14 @@ public class Character_Stunned : FSMState {
 	[System.NonSerialized]
 	public PlayerController Parent;
 
+	float stunDuration;
+	float startTime;
+
 	// Use this for initialization
-	public override void OnEnter () {
+	public override void OnEnter ( object userData ) {
 		Debug.Log( "Entered " + this );
+
+		stunDuration = (float)userData;
 	}
 
 	// Use this for initialization
@@ -18,8 +23,10 @@ public class Character_Stunned : FSMState {
 	}
 	
 	public override void OnUpdate(){
-		
+
+		//if we have been stunned long enough, go to previous state whatever that was...
+		if ( Time.time > startTime + stunDuration ){
+			Parent.GoToPreviousState();
+		}
 	}
-
-
 }

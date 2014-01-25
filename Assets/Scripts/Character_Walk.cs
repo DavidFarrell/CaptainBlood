b23b8b956b32ffc5dfe39b2d3198781d8b39211e
sharpoid <sharpoid@hotmail.co.uk>
2014-01-25 00:15:15 +0000

    First States Walk &  Idle

---
+++ Assets/Scripts/Character_Walk.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Character_Walk : FSMState {
+
+	[System.NonSerialized]
+	public PlayerController Parent;
+	
+	// Use this for initialization
+	public override void OnEnter () {
+		Debug.Log( "Entered " + this );
+	}
+	
+	// Update is called once per frame
+	public override void OnUpdate () {
+		
+		//retrieve axis info
+		Parent.horizAxis = Input.GetAxis("Horizontal1");
+		
+		if( Mathf.Abs( Parent.transform.rigidbody2D.velocity.x) > 0.6 ){
+			if (Mathf.Abs( Parent.transform.rigidbody2D.velocity.x) >= Parent.maxSpeed){
+				Parent.transform.rigidbody2D.velocity = new Vector2(Mathf.Sign( Parent.transform.rigidbody2D.velocity.x) * Parent.maxSpeed, Parent.transform.rigidbody2D.velocity.y);	// ... set the player's velocity to the maxSpeed in the x axis.
+			}
+			else{		// If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeed yet...
+				Parent.transform.rigidbody2D.AddForce(Vector2.right * Parent.horizAxis * Parent.moveForce);				// ... add a force to the player.
+			}
+		}
+		else{
+			//Parent.GoToState( Parent.s_idle );
+			if( Parent.horizAxis * Parent.transform.rigidbody2D.velocity.x < Parent.maxSpeed)
+			Parent.transform.rigidbody2D.AddForce(Vector2.right * Parent.horizAxis * Parent.moveForce);				// ... add a force to the player.
+		}
+		
+		
+//		if (Input.GetButtonDown ("Jump1") && grounded == true ){
+//				Parent.rigidbody2D.AddForce (Vector2.up * Parent.jumpForce);
+//		}
+		
+		// If the input is moving the player right and the player is facing left...
+		if( Parent.horizAxis > 0 && !Parent.facingRight){
+			// ... flip the player.
+			Parent.Flip();
+		}
+	
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : FSMSystem {
+	
+	//Add the states
+	public Character_Idle s_idle;
+	public Character_Walk s_walk;
+	
+	public bool facingRight = true;
+	public bool grounded;
+	public float horizAxis;
+	public float moveForce;
+	public float jumpForce;
+	public float maxSpeed;	
+	
+	void Awake(){
+		
+		AddState( s_idle );
+		s_idle.Parent = this;
+		
+		AddState (s_walk);
+		s_walk.Parent = this;
+		
+		//goto first default state
+		GoToState( s_idle );
+	}
+	
+	/// <summary>
+	/// Flip the direction character is facing
+	/// </summary>
+	public void Flip(){
+		facingRight = !facingRight;
+		Vector3 scale = transform.localScale;
+		scale.x *= -1;
+		transform.localScale = scale;
+	}
+}

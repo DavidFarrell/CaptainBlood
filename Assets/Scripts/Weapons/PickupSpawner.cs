b478a72d60d4f040ab86449e061b3d3664f43081
Colin Farrell <hiddeninjaskills@gmail.com>
2014-01-26 04:15:02 +0000

    Pickup spawner

---
+++ Assets/Scripts/Weapons/PickupSpawner.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupSpawner : MonoBehaviour {
+
+	public int frequency;
+	float spawnTime;
+
+	// Use this for initialization
+	void Start () {
+		spawnTime = Time.time;
+		GameObject newPickup = GameObject.Instantiate ( Resources.Load ("Prefabs/Pickup"), transform.position, Quaternion.identity) as GameObject;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Time.time > spawnTime + frequency){
+			//spawn a pickup
+			GameObject newPickup = GameObject.Instantiate ( Resources.Load ("Prefabs/Pickup"), transform.position, Quaternion.identity) as GameObject;
+			//reset timer
+			spawnTime = Time.time;
+
+			
+			
+		}
+	
+	}
+}

952ea3f5e290eaedc16fa1ea1de93925452d3d8c
DavidFarrell <dfarrell@davidlearnsgames.com>
2014-01-27 18:57:46 +0000

    Putt attract mode sound into game (badly)

--- Assets/Scripts/GameScreens/Game_Splash.cs
+++ Assets/Scripts/GameScreens/Game_Splash.cs
@@ -9,13 +9,18 @@ public class Game_Splash : FSMState {
 	public GUIText pressContinue;
 	private bool guiActive = true;
 	private float timer = 0;
+	public AudioEngine speaker;
+	private int tune;
 
 	public override void OnEnter () {
 		//GameObject newGameObject = GameObject.Instantiate (Resources.Load ("ScenePrefabs/ColinScenePrefab"), Vector3.zero, Quaternion.identity) as GameObject;
-
+		tune = 999;
 	}
 
 	public override void OnUpdate(){
+		if (tune == 999) {
+			tune = speaker.playSound (AudioEngine.SOUND_POSTER_ATTRACT_MODE, true);
+		}
 		timer += Time.deltaTime;
 		if (timer > 1.3){
 			if(guiActive){
@@ -31,6 +36,7 @@ public class Game_Splash : FSMState {
 		if(Input.anyKeyDown){
 			Debug.Log ("Splash: going to the next Level");
 			//Parent.NextLevel();
+			speaker.stopSound(tune);
 			Parent.GoToState(Parent.g_menu);
 			Application.LoadLevel("MainMenu");
 		}
